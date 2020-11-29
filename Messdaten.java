c9073d46c9419d9c02a830bf27103f92964ae590
yooorim <gyeyoorim.edu@gmail.com>
2020-11-29 19:37:58 +0100

    Checkstyle fixed, final

--- Messdaten.java
+++ Messdaten.java
@@ -2,51 +2,50 @@
  *@author ygye2s
  */
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Messdaten {
   public static double temperaturInDurchschnitt(Scanner sc) {
     String[] array;
     String line;
-    String tester = "";
 
-    int i = 0;
+    double highTem = -273.15; //min temperatur
+    double sumTem = 0; //variable for saving temperatures
+    int temCount = 0; //counter to get average temrerature
 
-    double high_tem = -273.15; //min temperatur
-    double sum_tem = 0; // variable for saving temperatures
-    int tem_count = 0; // counter to devide aver_tem
+    String message = "No Temperatur Input"; //default message for no temperature input
 
-    String message = "No Temperatur Information"; // default message for no temperature input
+    while (sc.hasNextLine()) {  //save scanner input by line and spaces
 
-    while (sc.hasNextLine()) {
       line = sc.nextLine();
-      line = line.replaceAll("\\s+"," ").trim();
+      line = line.replaceAll("\\s+", " ").trim();
       array = line.split(" ");
 
       double tem = Double.valueOf(array[4]);
 
       if (array[3].equals("Temperatur")) {
-        if(tem > high_tem){
-          high_tem = tem;
-          sum_tem = sum_tem + tem;
-          tem_count ++;
+        if (tem >= highTem) {
+
+          highTem = tem;
+          sumTem = sumTem + tem;
+          temCount++;
 
           String day = array[2];
           String month = array[1];
           String year = array[0];
+          message = "hoechste Temperatur (" + highTem + ") am " + day + "." + month + "." + year;
 
-          message = "hoechste Temperatur ("+ high_tem + ") am " + day + "." + month +"."+ year;
         } else {
-          sum_tem = sum_tem + tem;
-          tem_count++;
+          sumTem = sumTem + tem;
+          temCount++;
         }
-      } else { }
+      }
 
-      if (!sc.hasNext()) { break; }
+      if (!sc.hasNext()) {	//remove blank enter
+        break;
+      }
     }
-
     System.out.println(message);
-    return sum_tem / tem_count;
+    return sumTem / temCount;
   }
 }
