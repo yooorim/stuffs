3f6929fbc5f621abfb329d9c141993ccf23087b4
yooorim <gyeyoorim.edu@gmail.com>
2020-11-29 18:05:38 +0100

    윈도우로 옮길 용도

---
+++ a.java
@@ -0,0 +1,25 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+public class a  {
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        tem(scan);
+    }
+
+    public static double tem (Scanner scan) {
+        String[][] array = null;
+
+        while (scan.hasNextLine()){
+
+            String a = scan.nextLine();
+            a = a.trim();
+            String arr[] = a.split(" ");
+        }
+    }
+
+}
